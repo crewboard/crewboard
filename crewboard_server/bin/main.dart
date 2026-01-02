414f9585605364618784c3dd9a98816b19b2931f
Jerin <jerinjacob0007@gmail.com>
2026-01-02 13:24:44 +0530

    lint fix 5

--- crewboard_server/bin/main.dart
+++ crewboard_server/bin/main.dart
@@ -3,5 +3,6 @@ import 'package:crewboard_server/server.dart';
 /// This is the starting point for your Serverpod server. Typically, there is
 /// no need to modify this file.
 void main(List<String> args) {
+  //  This is a comment with too much space.
   run(args);
 }
